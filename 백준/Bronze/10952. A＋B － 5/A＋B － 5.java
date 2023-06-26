8ecb112c5a0b6784d3b9ee05c75fe4dd200c60dd
조씨 <73353809+caoyu-dev@users.noreply.github.com>
2023-06-26 23:48:28 +0900

    [Bronze V] Title: A+B - 5, Time: 216 ms, Memory: 17840 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input;
+
+        while (sc.hasNextLine() && !(input = sc.nextLine()).isEmpty() && !input.equals("0 0")) {
+            StringTokenizer st = new StringTokenizer(input);
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            System.out.println(n + m);
+        }
+        sc.close();
+    }
+}
