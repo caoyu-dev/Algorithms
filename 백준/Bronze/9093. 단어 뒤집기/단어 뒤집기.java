467c1d37b7b2fcb4b12ba543f2a753b4da095a02
조씨 <73353809+caoyu-dev@users.noreply.github.com>
2023-06-27 16:05:30 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 836 ms, Memory: 46928 KB -BaekjoonHub

---
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = sc.nextInt();
+        sc.nextLine();
+        while (T-- > 0) {
+            String str = sc.nextLine() + "\n";
+            Stack<Character> stack = new Stack<>();
+            for (char ch : str.toCharArray()) {
+                if (ch == '\n' || ch == ' ') {
+                    while (!stack.isEmpty()) {
+                        bw.write(stack.pop());
+                    }
+                    bw.write(ch);
+                } else {
+                    stack.push(ch);
+                }
+            }
+        }
+        bw.flush();
+    }
+}
