4bdb6437c86163ed87a0d3e93d6b71fe77f7a514
조씨 <73353809+caoyu-dev@users.noreply.github.com>
2023-09-15 22:14:56 +0900

    [Bronze V] Title: A/B, Time: 224 ms, Memory: 18000 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double n = sc.nextDouble();
+        double m = sc.nextDouble();
+        System.out.println(n / m);
+    }
+}
