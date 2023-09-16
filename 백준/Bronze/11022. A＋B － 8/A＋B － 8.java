5584dafac9f28cbbae78742d60966d0ed4dd0ff9
조씨 <73353809+caoyu-dev@users.noreply.github.com>
2023-09-16 21:31:39 +0900

    [Bronze V] Title: A+B - 8, Time: 268 ms, Memory: 18968 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -4,12 +4,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
+        int A, B;
+        int[] left = new int[T];
+        int[] right = new int[T];
 
         for (int i = 0; i < T; i++) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            int sum = n + m;
-            System.out.println("Case #" + (i + 1) + ": " + n + " + " + m + " = " + sum);
+            A = sc.nextInt();
+            B = sc.nextInt();
+            left[i] = A;
+            right[i] = B;
+        }
+
+        for (int i = 0; i < T; i++) {
+            System.out.println("Case #" + (i + 1) + ": " + left[i] + " + " + right[i] + " = " + (left[i] + right[i]));
         }
     }
 }
