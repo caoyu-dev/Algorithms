3ecd58c9449d7f8f605fa4e0bc047a151d0e7082
조씨 <73353809+caoyu-dev@users.noreply.github.com>
2023-09-17 13:40:39 +0900

    [Bronze V] Title: 개수 세기, Time: 220 ms, Memory: 18100 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,22 +1,19 @@
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[] list = new int[N];
-
-        for (int i = 0; i < N; i++) {
-            list[i] = sc.nextInt();
-        }
+        int[] list = IntStream.range(0, N)
+                .map(i -> sc.nextInt())
+                .toArray();
         int v = sc.nextInt();
-        int idx = 0;
 
-        for (int i = 0; i < N; i++) {
-            if (v == list[i]) {
-                idx++;
-            }
-        }
-        System.out.println(idx);
+        long count = IntStream.of(list)
+                .filter(num -> num == v)
+                .count();
+
+        System.out.println(count);
     }
 }
