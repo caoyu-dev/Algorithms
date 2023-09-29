8dffe016a7f214e79f4be9479abdaff4d2d20209
조씨 <73353809+caoyu-dev@users.noreply.github.com>
2023-09-30 01:14:06 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 224 ms, Memory: 18288 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+
+        for (int i = 'a'; i <= 'z'; i++) {
+            System.out.print(input.indexOf(i) + " ");
+        }
+    }
+}
